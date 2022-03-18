e4a0c5ebae6c1cd76bcf6b1fdd5edf30dc4a6109
Waldemar Teichreb <waldi.teichreb@gmail.com>
2022-03-18 14:15:16 +0100

    close to final

--- src/Components/addDrink.jsx
+++ src/Components/addDrink.jsx
@@ -8,24 +8,27 @@ const AddDrink = () => {
   return (
     <>
       <Header />
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="category" placeholder="Kategorie" />
-        <input type="text" name="image" placeholder="Bild URL" />
-        <input type="text" name="instruction" placeholder="Anleitung" />
-        <input type="text" name="ingredient1" placeholder="Zutat 1" />
-        <input type="text" name="measure1" placeholder="Menge Zutat 1" />
-        <input type="text" name="ingredient2" placeholder="Zutat 2" />
-        <input type="text" name="measure2" placeholder="Menge Zutat 2" />
-        <input type="text" name="ingredient3" placeholder="Zutat 3" />
-        <input type="text" name="measure3" placeholder="Menge Zutat 3" />
-        <input type="submit" name="submit" value="Submit" />
-      </form>
-      <figure>
-        <img src={image9} alt="-" />
-        <img src={image10} alt="-" />
-        <img src={image11} alt="-" />
-      </figure>
+      <h2>Füge deine eigenen Getränke hinzu!</h2>
+      <section className="add-drink-section">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input className="gin" type="text" name="name" placeholder="Name" />
+          <input className="vodka" type="text" name="category" placeholder="Kategorie" />
+          <input className="rum" type="text" name="image" placeholder="Bild URL" />
+          <textarea className="scotch" name="instruction" placeholder="Anleitung"></textarea>
+          <input className="alkoholfrei" type="text" name="ingredient1" placeholder="Zutat 1" />
+          <input className="zufall" type="text" name="measure1" placeholder="Menge Zutat 1" />
+          <input className="gin" type="text" name="ingredient2" placeholder="Zutat 2" />
+          <input className="vodka" type="text" name="measure2" placeholder="Menge Zutat 2" />
+          <input className="rum" type="text" name="ingredient3" placeholder="Zutat 3" />
+          <input className="scotch" type="text" name="measure3" placeholder="Menge Zutat 3" />
+          <input type="submit" name="submit" value="Submit" />
+        </form>
+        <figure>
+          <img src={image9} alt="-" />
+          <img src={image10} alt="-" />
+          <img src={image11} alt="-" />
+        </figure>
+      </section>
       <Footer />
     </>
   );
--- src/Components/searchField.jsx
+++ src/Components/searchField.jsx
@@ -1,8 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+
 const SearchField = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const getValue = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  let navigate = useNavigate();
+
+  const search = () => {
+    navigate(`/cocktailList/search.php%3Fs=${inputValue}`);
+  }
+
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-      <input type="text" name="text" placeholder="type something" />
-      <input type="submit" value="Search" />
+      <input onChange={getValue} type="text" name="text" placeholder="type something" />
+      <input onClick={search} type="button" value="Search" />
     </form>
   );
 }
